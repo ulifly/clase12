2c4be98686e705cda77ea946aca60b1616ab122a
ulifly <ulifly@hotmail.com>
2025-04-26 10:42:09 -0600

    haceindo fetch y fetch con axios

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-console.log("estamos probando github");
-console.log("estamos probando github 2");
-console.log("estamos probando github 3");   
+import axios from "axios";
+
+const URL = "https://futuramaapi.com/api/characters/1";
+
+// fetch(URL)
+//     .then((response) => {
+//         if (!response.ok) {
+//             throw new Error("error en la petición");
+//         }
+//         return response.json();
+//     })
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+
+axios.get(URL)
+    .then(respuesta => {
+        console.log(respuesta.data);
+    })
+    .catch(err => {
+        console.log(err);
+    });
